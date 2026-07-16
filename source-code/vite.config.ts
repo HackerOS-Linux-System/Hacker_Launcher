e8bf3717cb6d6e2021c9419c47e5f5fcad763ff9
michal92299 <chudybyczek2137@gmail.com>
2026-07-16 18:55:22 +0200

    Update vite.config.ts

--- source-code/vite.config.ts
+++ source-code/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import solid from "vite-plugin-solid";
 
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [solid()],
   clearScreen: false,
   server: {
     port: 1420,
